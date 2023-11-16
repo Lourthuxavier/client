432e91e29714ba097ab2e779e00baf2998c151d2
Lourthuxavier <lourthuxavier.m@getster.tech>
2023-11-16 10:02:37 +0530

    powereb by removed

--- src/app/modules/project-name/mathtype/mathtype.component.ts
+++ src/app/modules/project-name/mathtype/mathtype.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Editor from '../../../../../CKEditor5-custom-build/build/ckeditor';
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 @Component({
-  selector: 'app-mathtype',
+  selector: '#app-mathtype',
   templateUrl: './mathtype.component.html',
   styleUrls: ['./mathtype.component.scss']
 })

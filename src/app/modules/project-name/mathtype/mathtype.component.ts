45236c9251b97dd474b1214f33dbe70f0978ee71
Lourthuxavier <lourthuxavier.m@getster.tech>
2023-11-16 12:13:16 +0530

    responsive changed for large screen

--- src/app/modules/project-name/mathtype/mathtype.component.ts
+++ src/app/modules/project-name/mathtype/mathtype.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import Editor from '../../../../../CKEditor5-custom-build/build/ckeditor';
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 @Component({
   selector: '#app-mathtype',
   templateUrl: './mathtype.component.html',
-  styleUrls: ['./mathtype.component.scss']
+  styleUrls: ['./mathtype.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MathtypeComponent implements OnInit {
 
